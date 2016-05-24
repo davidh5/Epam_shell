5356be28661ae0b1254ad9b6c025746161a55b5a
davidh5 <holkodavid@gmail.com>
2016-05-24 19:36:03 +0200

    complete handling of prompt command

--- src/Shell.java
+++ src/Shell.java
@@ -1,31 +1,75 @@
+import java.io.File;
 import java.util.Scanner;
-
-/**
- * 
- */
 
 /**
  * @author Dawid
  *
  */
 public class Shell {
+	
+	private static final String EXIT_COMMAND = "exit";
+	private static final String PROMPT_COMMAND = "prompt";
+	private static final String PROMPT_RESET_COMMAND = "reset";
+	private static final String PROMPT_CWD_COMMAND = "$cwd";
+	private static final String PROMPT_DEFUALT_SIGN = "$";
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Prompt prompt = new Prompt("$");
-		String text = "";
-		Scanner consoleReader = new Scanner(System.in);
-		while (!(text.equals("exit"))) {
-			System.out.print("Type command: ");
-			text = consoleReader.nextLine();
-			System.out.println(text);
-		}
-		System.out.println("Application is closing!");
-		System.exit(0);
 		
+		String text = "";
+		String[] commandParts;
+		Scanner consoleReader = new Scanner(System.in);
+		
+		while (!(text.equals(EXIT_COMMAND))) {
+			
+			System.out.print(prompt.getPromptSign()+" ");
+			
+			text = consoleReader.nextLine();
+			commandParts = text.split(" ");
+			
+			switch (commandParts[0]) {
+			
+			case PROMPT_COMMAND:
+				if (commandParts.length == 1 || commandParts.length >2) {
+					System.out.println("'Prompt' command parameters are wrong");
+					break;
+				} else {
+					promptChange(prompt, commandParts[1]);
+					break;
+				}
+				
+			case EXIT_COMMAND:
+				System.out.println("Shell is closing!");
+				System.exit(0);
+				break;
+				
+			default:
+				System.out.println(commandParts[0]+":unknown command");
+				break;
+			}
+		}
 	}
-
+	
+	/**
+	 * @param prompt Prompt object which defines the prompt sign in shell
+	 * @param parameter Second part of typed command
+	 */
+	public static void promptChange(Prompt prompt, String parameter) {
+		
+		switch (parameter) {
+		
+		case PROMPT_RESET_COMMAND:
+			prompt.setPromptSign(PROMPT_DEFUALT_SIGN);
+			break;
+		
+		case PROMPT_CWD_COMMAND:
+			File fileToObtainPath = new File("");
+			prompt.setPromptSign(fileToObtainPath.getAbsolutePath());
+			break;
+		
+		default:
+			prompt.setPromptSign(parameter);
+			break;
+		}
+	}
 }
