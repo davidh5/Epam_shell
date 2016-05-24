358d2a4939d19ef94b028b314e8f408b4c4d211f
davidh5 <holkodavid@gmail.com>
2016-05-24 17:41:10 +0200

    just a model of application

---
+++ src/Prompt.java
@@ -0,0 +1,27 @@
+
+public class Prompt {
+	
+	private String promptSign;
+	
+	public Prompt(String promptSign) {
+		super();
+		this.promptSign = promptSign;
+	}
+	
+	/**
+	 * @return the promptSign
+	 */
+	public String getPromptSign() {
+		return promptSign;
+	}
+
+	/**
+	 * @param promptSign the promptSign to set
+	 */
+	public void setPromptSign(String promptSign) {
+		this.promptSign = promptSign;
+	}
+
+
+	
+}
