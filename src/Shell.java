f8b083974436e8c386d99ed73214ee4ff5edbd29
davidh5 <holkodavid@gmail.com>
2016-05-26 00:37:42 +0200

    done tree command

--- src/Shell.java
+++ src/Shell.java
@@ -10,6 +10,8 @@ public class Shell {
 	
 	private static final String EXIT_COMMAND = "exit";
 	private static final String DIR_COMMAND = "dir";
+	private static final String TREE_COMMAND = "tree";
+	private static final String CD_COMMAND = "cd";
 	private static final String PROMPT_COMMAND = "prompt";
 	private static final String PROMPT_RESET_COMMAND = "reset";
 	private static final String PROMPT_CWD_COMMAND = "$cwd";
@@ -46,12 +48,21 @@ public class Shell {
 				if (commandParts.length != 1) {
 					System.out.println("'Dir' command should be run without any parameters");
 				} else {
-					try {
-						showDirectoryContent(currentDirectory);
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
+					showDirectoryContent(currentDirectory);
 				}
+				break;
+				
+			case TREE_COMMAND:
+				if (commandParts.length != 1) {
+					System.out.println("'Tree' command should be run without any parameters");
+				} else {
+					int indent = 0;
+					showDirectoryTree(currentDirectory, indent);
+				}
+				break;
+				
+			case CD_COMMAND:
+				changeDirectory(currentDirectory, commandParts[1]);
 				break;
 				
 			case EXIT_COMMAND:
@@ -62,7 +73,7 @@ public class Shell {
 			
 				
 			default:
-				System.out.println(commandParts[0]+":unknown command");
+				System.out.println(commandParts[0]+" : unknown command");
 				break;
 			}
 		}
@@ -74,7 +85,7 @@ public class Shell {
 	 * @param parameter Second part of typed command
 	 * @param directory Current working directory, it is used to show current directory path in prompt
 	 */
-	public static void promptChange(Prompt prompt, String parameter, File directory) {
+	private static void promptChange(Prompt prompt, String parameter, File directory) {
 		
 		switch (parameter) {
 		
@@ -83,7 +94,11 @@ public class Shell {
 			break;
 		
 		case PROMPT_CWD_COMMAND:
-			prompt.setPromptSign(directory.getAbsolutePath());
+			try {
+				prompt.setPromptSign(directory.getCanonicalPath());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 			break;
 		
 		default:
@@ -96,16 +111,16 @@ public class Shell {
 	 * @param directory Directory which content should be displayed
 	 *
 	 */
-	public static void showDirectoryContent(File directory) throws IOException {
+	private static void showDirectoryContent(File directory) {
 		
 		File[] files = directory.listFiles();
+		
 		try {
-		System.out.println("Content of: " + directory.getCanonicalPath());
-		} catch (IOException ex) {
-			ex.printStackTrace();
+			System.out.println("Content of " + directory.getCanonicalPath());
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		
-			
 		for (File f : files) {
 			if (f.isDirectory()) {
 				System.out.println("DIR \t" + f.getName());
@@ -113,6 +128,41 @@ public class Shell {
 				System.out.println("FILE \t" + f.getName());
 			}
 		}
+		
+	}
+	
+	/**
+	 * @param directory Directory which content should be displayed
+	 * @param indent Variable describing in which depth we are in directories tree
+	 */
+	private static void showDirectoryTree(File directory, int indent) {
+		if (indent == 0) {
+			try {
+				String pathOfCurrentDirectory = directory.getCanonicalPath();
+				String nameOfCurrentDirectory = pathOfCurrentDirectory.substring(pathOfCurrentDirectory.lastIndexOf("\\")+1);
+				System.out.println(nameOfCurrentDirectory);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		File[] directories = directory.listFiles();
+		for (File f : directories) {
+			if (f.isDirectory()) {
+				getIndentString(indent);
+				System.out.println(f.getName());
+				showDirectoryTree(f, indent+1);
+			}
+		}
+		
+	}
+	
+	private static void getIndentString(int indent) {
+	    for (int i = 0; i <= indent; i++) {
+	    	System.out.print('-');
+	    }
+	}
+	
+	private static void changeDirectory(File directory, String parameter) {
 		
 	}
 }
